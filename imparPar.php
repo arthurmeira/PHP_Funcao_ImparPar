b9e064764b37ec8849e7f6016296536d98836ed7
Arthur Meria <85205144+arthurmeira@users.noreply.github.com>
2021-06-19 10:19:56 -0300

    Create imparPar.php

---
+++ imparPar.php
@@ -0,0 +1,19 @@
+<?php
+
+//Declarando a Função
+function impar_par($numero)
+{
+/*Se usa % que substitui o MOD no PHP, 
+para capturar o resto ou módulo da divisão*/
+    if($impar = $numero % 2){
+        echo "$numero é IMPAR!";
+    }else{
+        echo "$numero é PAR!";
+    }
+}
+/*$num é onde armazenamos o número em que vamos 
+usar para testar se é um valor impar ou par*/
+$num = 4;
+impar_par($num);
+
+?>
